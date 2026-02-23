e255fa38bf846f97496d1eb5d2d1ef07f91581cb
Vinoth Kumar <vasuvino36@gmail.com>
2026-02-23 17:26:54 +0530

    commit

--- moremarket/adminpanel/urls.py
+++ moremarket/adminpanel/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signup_view, name='signup'),
+    # Authentication
     path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout_view, name='logout'),
+
+    # Dashboard
     path('dashboard/', views.admin_dashboard, name='dashboard'),
+
+    # Example Protected Page
+    path('nono/', views.nono_view, name='nono'),
 ]
