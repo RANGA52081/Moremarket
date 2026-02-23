c75239b13ceaf0b3ab8a3d3060c403171a94e21d
RANGA52081 <shajahaan403@gmail.com>
2026-02-23 15:26:54 +0530

    commit

--- moremarket/customer/views.py
+++ moremarket/customer/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Banner
 
 def customer_home(request):
     return render(request, 'customer/customer.html')
+def customer_home(request):
+    banners = Banner.objects.filter(is_active=True)
+    return render(request, 'customer/customer.html', {
+        'banners': banners
+    })
--- moremarket/moremarket/urls.py
+++ moremarket/moremarket/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('', include('customer.urls')),
     path('adminpanel/', include('adminpanel.urls')),
     path('analytics/', include('analytics.urls')),
     path('delivery/', include('delivery.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
