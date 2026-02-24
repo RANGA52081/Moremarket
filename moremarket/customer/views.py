24bced6524248aeb07ad48c8b8169d01df8529af
RANGA52081 <shajahaan403@gmail.com>
2026-02-24 11:22:43 +0530

    commit

--- moremarket/customer/views.py
+++ moremarket/customer/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from .models import Banner
 
 def customer_home(request):
     return render(request, 'customer/customer.html')
+from .models import Banner
+
 def customer_home(request):
     banners = Banner.objects.filter(is_active=True)
     return render(request, 'customer/customer.html', {
