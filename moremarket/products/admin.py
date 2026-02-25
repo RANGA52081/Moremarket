4bfa6ef8c5e1b7eac5ee6146f801fda5284f7d79
RANGA52081 <shajahaan403@gmail.com>
2026-02-25 13:09:49 +0530

    commit

--- moremarket/products/admin.py
+++ moremarket/products/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Product, ProductVariant
 
-# Register your models here.
+
+class ProductVariantInline(admin.TabularInline):
+    model = ProductVariant
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "is_featured", "created_at")
+    list_filter = ("category", "is_featured")
+    search_fields = ("name",)
+    inlines = [ProductVariantInline]
+
+
+@admin.register(ProductVariant)
+class ProductVariantAdmin(admin.ModelAdmin):
+    list_display = ("product", "size", "weight", "price")
