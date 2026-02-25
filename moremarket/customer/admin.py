87af414fd06152aca01ee7cf52aff93d78c370f4
RANGA52081 <shajahaan403@gmail.com>
2026-02-25 12:25:35 +0530

    commit

--- moremarket/customer/admin.py
+++ moremarket/customer/admin.py
@@ -1,4 +1,24 @@
 from django.contrib import admin
-from .models import Banner
+from .models import Banner, Product, Address, UserOTP
 
-admin.site.register(Banner)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "price")
+    list_filter = ("category",)
+    search_fields = ("name",)
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ("user", "full_name", "city", "pincode")
+
+
+@admin.register(Banner)
+class BannerAdmin(admin.ModelAdmin):
+    list_display = ("title", "is_active")
+
+
+@admin.register(UserOTP)
+class UserOTPAdmin(admin.ModelAdmin):
+    list_display = ("user", "otp", "created_at")
