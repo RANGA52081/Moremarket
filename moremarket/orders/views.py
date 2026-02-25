cc938006dbf4543c5b08697bd77f8281a048716e
RANGA52081 <shajahaan403@gmail.com>
2026-02-25 13:25:18 +0530

    commit

--- moremarket/orders/views.py
+++ moremarket/orders/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from products.models import ProductVariant
+from .models import Cart, CartItem
 
-# Create your views here.
+
+@login_required
+def add_to_cart(request, variant_id):
+    variant = get_object_or_404(ProductVariant, id=variant_id)
+
+    cart, created = Cart.objects.get_or_create(user=request.user)
+
+    cart_item, created = CartItem.objects.get_or_create(
+        cart=cart,
+        variant=variant
+    )
+
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
+
+    return redirect("orders:cart_view")
+
+
+@login_required
+def cart_view(request):
+    cart = Cart.objects.filter(user=request.user).first()
+    return render(request, "orders/cart.html", {"cart": cart})
