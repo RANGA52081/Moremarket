37581059ca3bd2af2c952bf7ffc39978478baf3d
RANGA52081 <shajahaan403@gmail.com>
2026-02-25 13:12:32 +0530

    commit

--- moremarket/customer/models.py
+++ moremarket/customer/models.py
@@ -31,20 +31,7 @@ class Address(models.Model):
 
     def __str__(self):
         return f"{self.city} - {self.pincode}"
-class Product(models.Model):
 
-    CATEGORY_CHOICES = (
-        ("MS", "MS Scaffolding"),
-        ("AL", "Aluminium Scaffolding"),
-    )
-
-    name = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to="products/")
-    category = models.CharField(max_length=5, choices=CATEGORY_CHOICES)
-
-    def __str__(self):
-        return self.name
     
 class UserOTP(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
