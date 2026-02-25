4dbdae97a0e2198f3b529641e426b9305fc21ace
Vinoth Kumar <vasuvino36@gmail.com>
2026-02-25 15:14:14 +0530

    commit

--- moremarket/moremarket/settings.py
+++ moremarket/moremarket/settings.py
@@ -77,16 +77,27 @@ WSGI_APPLICATION = 'moremarket.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'moremarket_db',     # Database name you created
-        'USER': 'postgres',         # Default user
-        'PASSWORD': 'post@26',  # The password you set
-        'HOST': 'localhost',
-        'PORT': '5432',
+import os
+
+if os.environ.get('USE_POSTGRES'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'moremarket_db',
+            'USER': 'postgres',
+            'PASSWORD': 'post@26',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+    
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
