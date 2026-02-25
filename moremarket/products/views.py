31738634edc981c8c3354f54f394059ceb286dd6
RANGA52081 <shajahaan403@gmail.com>
2026-02-25 13:10:22 +0530

    commit

--- moremarket/products/views.py
+++ moremarket/products/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
-# Create your views here.
+
+# 🔹 All Products Page
+def product_list(request):
+    category = request.GET.get("category")
+
+    if category:
+        products = Product.objects.filter(category=category)
+    else:
+        products = Product.objects.all()
+
+    return render(request, "products/product_list.html", {
+        "products": products
+    })
+
+
+# 🔹 Product Detail Page
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    variants = product.variants.all()
+
+    return render(request, "products/product_detail.html", {
+        "product": product,
+        "variants": variants
+    })
