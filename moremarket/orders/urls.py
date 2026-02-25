3b18ff7d0da497f537eb59abffa712cf80b2b027
RANGA52081 <shajahaan403@gmail.com>
2026-02-25 13:25:58 +0530

    commit

---
+++ moremarket/orders/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = "orders"
+
+urlpatterns = [
+    path("cart/", views.cart_view, name="cart_view"),
+    path("add-to-cart/<int:variant_id>/", views.add_to_cart, name="add_to_cart"),
+]
