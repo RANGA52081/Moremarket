1965c0b96deab8734504defe8a9b01adceadbf14
RANGA52081 <shajahaan403@gmail.com>
2026-02-28 11:02:48 +0530

    commit

--- moremarket/orders/urls.py
+++ moremarket/orders/urls.py
@@ -6,4 +6,6 @@ app_name = "orders"
 urlpatterns = [
     path("cart/", views.cart_view, name="cart_view"),
     path("add-to-cart/<int:variant_id>/", views.add_to_cart, name="add_to_cart"),
+    path("update-quantity/<int:item_id>/", views.update_quantity, name="update_quantity"),
+    path("remove-item/<int:item_id>/", views.remove_item, name="remove_item"),
 ]
--- moremarket/orders/views.py
+++ moremarket/orders/views.py
@@ -2,24 +2,31 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from products.models import ProductVariant
 from .models import Cart, CartItem
-
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 @login_required
 def add_to_cart(request, variant_id):
-    variant = get_object_or_404(ProductVariant, id=variant_id)
 
+    variant = get_object_or_404(ProductVariant, id=variant_id)
     cart, created = Cart.objects.get_or_create(user=request.user)
 
-    cart_item, created = CartItem.objects.get_or_create(
+    item, created = CartItem.objects.get_or_create(
         cart=cart,
         variant=variant
     )
 
     if not created:
-        cart_item.quantity += 1
-        cart_item.save()
+        item.quantity += 1
+        item.save()
 
-    return redirect("orders:cart_view")
+    if request.headers.get("x-requested-with") == "XMLHttpRequest":
+        return JsonResponse({
+            "cart_count": cart.items.count(),
+            "cart_total": cart.total_price()
+        })
+
+    return redirect("cart")
 
 
 @login_required
@@ -29,4 +36,33 @@ def cart_view(request):
 
     return render(request, "orders/cart.html", {
         "cart": cart
+    })
+
+@require_POST
+def update_quantity(request, item_id):
+
+    item = get_object_or_404(CartItem, id=item_id, cart__user=request.user)
+
+    quantity = int(request.POST.get("quantity"))
+
+    if quantity > 0:
+        item.quantity = quantity
+        item.save()
+
+    return JsonResponse({
+        "item_total": item.total_price,
+        "cart_total": item.cart.total_price()
+    })
+
+
+@require_POST
+def remove_item(request, item_id):
+
+    item = get_object_or_404(CartItem, id=item_id, cart__user=request.user)
+    cart = item.cart
+    item.delete()
+
+    return JsonResponse({
+        "cart_total": cart.total_price(),
+        "cart_count": cart.items.count()
     })
